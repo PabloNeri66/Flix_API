b3b3e3123e15a64a4738856f8bffd2fe31acf6a7
Pablo Alves Neri <pabloneri66@gmail.com>
2025-09-03 18:34:13 -0300

    feat: Validators e Rate logic Serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -4,6 +4,22 @@ from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
 
+    rate = serializers.SerializerMethodField(read_only=True)
+
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1935:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1935.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Ultrapassou o limite de 200 caracteres.')
+        return value
+
+    def get_rate(self, obj):
+        reviews = obj.reviews.all()
