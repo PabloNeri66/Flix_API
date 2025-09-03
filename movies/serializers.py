aaee0d0b0edf817da53d3705fc507ca06d53eb2b
Pablo Alves Neri <pabloneri66@gmail.com>
2025-09-03 20:13:55 -0300

    feat FAQ Dúvidas + get_rate Serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from movies.models import Movie
 
-
 class MovieSerializer(serializers.ModelSerializer):
 
     rate = serializers.SerializerMethodField(read_only=True)
@@ -11,15 +10,22 @@ class MovieSerializer(serializers.ModelSerializer):
         model = Movie
         fields = '__all__'
 
+
     def validate_release_date(self, value):
         if value.year < 1935:
             raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1935.')
         return value
     
+
     def validate_resume(self, value):
         if len(value) > 200:
             raise serializers.ValidationError('Ultrapassou o limite de 200 caracteres.')
         return value
 
+
     def get_rate(self, obj):
         reviews = obj.reviews.all()
+        if reviews:
+            obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        return None
