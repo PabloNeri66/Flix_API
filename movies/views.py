b0d7c0f5e4e54d44fd6b07fc42d1abc23ab94b02
Pablo Alves Neri <pabloneri66@gmail.com>
2025-09-09 18:06:11 -0300

    flake8

--- movies/views.py
+++ movies/views.py
@@ -3,28 +3,42 @@ from movies.models import Movie
 from movies.serializers import MovieSerializer
 from rest_framework.permissions import IsAuthenticated
 from core.permissions import GlobalDefaultPermission
+from django.db.models import Count, Avg
+from reviews.models import Review
+
 
 class MovieCreateListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes=(IsAuthenticated, GlobalDefaultPermission)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
 
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes=(IsAuthenticated, GlobalDefaultPermission)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
 
 
 class MovieStatsView(views.APIView):
-    permission_classes=(IsAuthenticated, GlobalDefaultPermission,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
 
     def get(self, request):
-        #buscar todos os dados
-        #montar resposta
-        #retornar resposta
+        # buscar todos os dados
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(
+            count=Count('id')
+        )
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(
+            avg_stars=Avg('stars'))['avg_stars']
+
         return response.Response(
-            data={'message':'Funfo'},
-            status=status.HTTP_200_OK
+            data={
+                "total_movies": total_movies,
+                "movies_by_genre": movies_by_genre,
+                "total_reviews": total_reviews,
+                "average_stars": round(average_stars, 1) if average_stars else 0,
+            },
+            status=status.HTTP_200_OK,
         )
